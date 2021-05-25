48059ee6b0cced93f07e71c54642b211a6776c40
zimmed <zimmed@zimmed.io>
2021-05-24 23:22:40 -0400

    Import try

--- src/build/getPages.ts
+++ src/build/getPages.ts
@@ -35,7 +35,7 @@ export default async function getPages(pagesPath: string): Promise<RecursiveCMap
       let mod;
 
       if (/\.(t|j)sx?$/i.test(file.name)) {
-        mod = await import(relative(__dirname, join(path, file.name)));
+        mod = await import('!!raw-loader!./' + relative(process.cwd(), join(path, file.name)));
         Comp = mod?.default || mod;
         if (!Comp) throw new Error(`File export is empty: ${join(path, file.name)}`);
         if (Comp.renderable) {
@@ -54,7 +54,7 @@ export default async function getPages(pagesPath: string): Promise<RecursiveCMap
         Comp = createComponent(
           name,
           convertInterpolation(
-            await import(relative(__dirname, join(path, file.name))),
+            await import('!!raw-loader!./' + relative(process.cwd(), join(path, file.name))),
             // fs.readFileSync(join(path, file.name), 'utf8')?.toString(),
             'hbs',
             'node'
